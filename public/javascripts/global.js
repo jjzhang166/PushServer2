0cd02e83678c90f8a32d58940e6e0b4d498acf45
Junggoo Kang <jgookang@gmail.com>
2014-07-25 18:05:27 +0900

    Initail commit.

---
+++ public/javascripts/global.js
@@ -0,0 +1,105 @@
+/**
+ * Created by AP_Java on 14. 7. 25..
+ */
+// Userlist data array for filling in info box
+var userListData = [];
+
+// DOM Ready =============================================================
+$(document).ready(function() {
+
+    // Populate the user table on initial page load
+    populateTable();
+
+});
+
+// Functions =============================================================
+
+// Fill table with data
+function populateTable() {
+
+    // Empty content string
+    var tableContent = '';
+
+    // jQuery AJAX call for JSON
+    $.getJSON( '/users/userlist', function( data ) {
+
+        userListData = data;
+
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function(){
+            tableContent += '<tr>';
+            tableContent += '<td><a href="#" class="linkshowuser rel="' + this.register_id + '" title="Show Details">' + this.register_id + '</td>';
+            tableContent += '<td>' + this.phone_number + '</td>';
+            tableContent += '<td>' + this.Phone_id + '</td>';
+            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '</tr>';
+        });
+
+        // Inject the whole content string into our existing HTML table
+        $('#userList table tbody').html(tableContent);
+        // Username link click
+        $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+        // Delete User link click
+        $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+    });
+};
+
+
+// Show User Info
+function showUserInfo(event) {
+
+    // Prevent Link from Firing
+    event.preventDefault();
+
+    // Retrieve username from link rel attribute
+    var thisUserName = $(this).attr('rel');
+
+    // Get Index of object based on id value
+    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.register_id; }).indexOf(thisUserName);
+    // Get our User Object
+    var thisUserObject = userListData[arrayPosition];
+
+    //Populate Info Box
+    $('#RegisterID').text(thisUserObject.register_id);
+    $('#PhoneNumber').text(thisUserObject.phone_number);
+    $('#PhoneID').text(thisUserObject.gender);
+};
+
+// Delete User
+function deleteUser(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
+};
